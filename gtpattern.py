7cd33dfc4168e0ffb37c673ac55a8b9f28d596a8
dumblearner <31450880+dumblearner@users.noreply.github.com>
2017-09-15 22:53:36 +0530

    Update gtpattern.py

--- gtpattern.py
+++ gtpattern.py
@@ -50,3 +50,27 @@ class gtpattern:
             for i in range(l-j-1,-1,-1):
                 w[j] = w[j]+[(l-i)]*int(self.wmatrix[i][j])
         return(w)
+
+    def from_matrix(a):
+        """
+        Create a Gelfand-Tsetlin pattern from a numpy array ``a``.
+        Example:
+            >>> a = numpy.array([[3,2,1],[2,1,0],[1,0,0]])
+            >>> gtpattern.from_matrix(a)
+            [[3, 2, 1], [2, 1], [1]]
+
+        """
+        assert isinstance(a, np.ndarray) and (a.shape[0] == a.shape[1])
+        assert all(a[i-1][j] >= a[i][j] >= a[i-1][j+1] for i in range(1, len(a)) for j in range(len(a)-i))
+        p = gtpattern([[]])
+        p.matrix = a
+        p.wmatrix = np.zeros(p.matrix.shape)
+        for i in range(len(p.matrix)-1):
+            p.wmatrix[i] = p.matrix[i] - p.matrix[i+1]
+        p.wmatrix[-1] = p.matrix[-1]
+        li = []
+        l = len(a)
+        for i in range(l):
+            li.append(list(a[i,:l-i]))
+        p.gt = li
+        return p
